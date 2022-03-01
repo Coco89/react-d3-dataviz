0cbf72367b989a9f284b7ddcea49caae95117923
Chris Pascual <christopher.pascual@phil.frb.org>
2022-02-28 20:38:34 -0500

    Fixed issue with multiple chart renders

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import Chart from "./Chart";
 import Column from "./Column";
 
 function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = React.useState(true);
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     d3.json(
       "https://www.ncdc.noaa.gov/cag/global/time-series/globe/land_ocean/1/10/1880-2020/data.json"
-    ).then((data) => {
-      setData(data);
+    ).then((data2) => {
+      setData(data2);
       setLoading(false);
     });
-    return () => undefined;
-  }, [data]);
+    // return () => undefined;
+  }, []);
 
   return (
     <div className="container">
--- src/Chart.js
+++ src/Chart.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 import "./Chart.css";
 
@@ -90,7 +90,15 @@ function update(prep, val) {
 }
 
 function Chart({ data }) {
+  const svgRef = useRef();
   const heading = data.description.title;
+  const url =
+    "https://www.ncdc.noaa.gov/cag/global/time-series/globe/land_ocean/1/10/1880-2020/data.json";
+
+  // labels
+  const x_label = "Year";
+  const y_label = "Temperature";
+  const location_name = heading;
 
   const onChange = (event) => {
     console.log("inside Chart on change event: ", event);
@@ -108,143 +116,137 @@ function Chart({ data }) {
     // }
   };
 
-  const url =
-    "https://www.ncdc.noaa.gov/cag/global/time-series/globe/land_ocean/1/10/1880-2020/data.json";
+  useEffect(() => {
 
-  // labels
-  const x_label = "Year";
-  const y_label = "Temperature";
-  const location_name = heading;
 
-  // seeting margin, width, and height
-  const margin = { left: 120, right: 30, top: 60, bottom: 30 },
-    width = document.querySelector("body").clientWidth,
-    height = 1250;
+    // seeting margin, width, and height
+    const margin = { left: 120, right: 30, top: 60, bottom: 30 },
+      width = document.querySelector("body").clientWidth,
+      height = 1250;
 
-  // render svg
-  const svg = d3.select("svg").attr("viewBox", [0, 0, width, height]);
+    // render svg
+    const svg = d3.select(svgRef.current).attr("viewBox", [0, 0, width, height]);
 
-  // add title
-  svg
-    .append("text")
-    .attr("class", "svg_title")
-    .attr("x", (width - margin.right + margin.left) / 2)
-    .attr("y", margin.top / 2)
-    .attr("text-anchor", "middle")
-    .style("font-size", "22px")
-    .text(`${y_label} of ${location_name}`);
+    // add title
+    svg
+      .append("text")
+      .attr("class", "svg_title")
+      .attr("x", (width - margin.right + margin.left) / 2)
+      .attr("y", margin.top / 2)
+      .attr("text-anchor", "middle")
+      .style("font-size", "22px")
+      .text(`${y_label} of ${location_name}`);
 
-  // add y label
-  svg
-    .append("text")
-    .attr("text-ancho", "middle")
-    .attr(
-      "transform",
-      `translate(${margin.left - 70}, ${
-        (height - margin.top - margin.bottom + 180) / 2
-      }) rotate(-90)`
-    )
-    .style("font-size", "26px")
-    .text(y_label);
+    // add y label
+    svg
+      .append("text")
+      .attr("text-ancho", "middle")
+      .attr(
+        "transform",
+        `translate(${margin.left - 70}, ${(height - margin.top - margin.bottom + 180) / 2
+        }) rotate(-90)`
+      )
+      .style("font-size", "26px")
+      .text(y_label);
 
-  // add x label
-  svg
-    .append("text")
-    .attr("class", "svg_title")
-    .attr("x", (width - margin.right + margin.left) / 2)
-    .attr("y", height - margin.bottom - margin.top + 60)
-    .attr("text-anchor", "middle")
-    .style("font-size", "26px")
-    .text(x_label);
+    // add x label
+    svg
+      .append("text")
+      .attr("class", "svg_title")
+      .attr("x", (width - margin.right + margin.left) / 2)
+      .attr("y", height - margin.bottom - margin.top + 60)
+      .attr("text-anchor", "middle")
+      .style("font-size", "26px")
+      .text(x_label);
 
-  const x_scale = d3.scaleLinear().range([margin.left, width - margin.right]);
-  const y_scale = d3
-    .scaleLinear()
-    .range([height - margin.bottom - margin.top, margin.top]);
+    const x_scale = d3.scaleLinear().range([margin.left, width - margin.right]);
+    const y_scale = d3
+      .scaleLinear()
+      .range([height - margin.bottom - margin.top, margin.top]);
 
-  // ticks: number of ticks displayed
-  const ticks = 10;
+    // ticks: number of ticks displayed
+    const ticks = 10;
 
-  const x_axis = d3
-    .axisBottom()
-    .scale(x_scale)
-    .tickPadding(10)
-    .ticks(ticks)
-    .tickSize(-height + margin.top * 2 + margin.bottom);
+    const x_axis = d3
+      .axisBottom()
+      .scale(x_scale)
+      .tickPadding(10)
+      .ticks(ticks)
+      .tickSize(-height + margin.top * 2 + margin.bottom);
 
-  const y_axis = d3
-    .axisLeft()
-    .scale(y_scale)
-    .tickPadding(5)
-    .ticks(ticks, ".1")
-    .tickSize(-width + margin.left + margin.right);
+    const y_axis = d3
+      .axisLeft()
+      .scale(y_scale)
+      .tickPadding(5)
+      .ticks(ticks, ".1")
+      .tickSize(-width + margin.left + margin.right);
 
-  // convert y axis to have 2 decimal places
-  y_axis.tickFormat((d) => {
-    return d3.format(".2f")(d / 1);
-  });
-
-  // convert x axis to have no comma
-  x_axis.tickFormat((d) => {
-    return d3.format("")(d / 1);
-  });
-
-  const year = (d) => d.year;
-  const temperature = (d) => d.temp;
-
-  const line_generator = d3
-    .line()
-    .x((d) => x_scale(year(d)))
-    .y((d) => y_scale(temperature(d)))
-    .curve(d3.curveBasis);
-
-  d3.json(url).then(({ data }) => {
-    let temperatures = [];
-
-    Object.keys(data).map((key, i) => {
-      const year = key;
-      const temp = data[key];
-
-      temperatures.push({ year: year, temp: temp });
+    // convert y axis to have 2 decimal places
+    y_axis.tickFormat((d) => {
+      return d3.format(".2f")(d / 1);
     });
 
-    const d = temperatures;
+    // convert x axis to have no comma
+    x_axis.tickFormat((d) => {
+      return d3.format("")(d / 1);
+    });
 
-    //min and max for
-    let minYear = d3.min(temperatures, (t) => t.year);
-    let maxYear = d3.max(temperatures, (t) => t.year);
+    const year = (d) => d.year;
+    const temperature = (d) => d.temp;
 
-    // x_scale.domain(d3.extent(d, year)).nice(ticks);
-    x_scale.domain([minYear, maxYear]).nice(ticks);
+    const line_generator = d3
+      .line()
+      .x((d) => x_scale(year(d)))
+      .y((d) => y_scale(temperature(d)))
+      .curve(d3.curveBasis);
 
-    // y_scale.domain(d3.extent(d, temperature)).nice(ticks);
-    y_scale.domain([-0.6, d3.max(temperatures, (t) => t.temp)]).nice(ticks);
+      let temperatures = [];
 
-    // append x axis
-    svg
-      .append("g")
-      .attr("transform", `translate(0,${height - margin.bottom - margin.top})`)
-      .call(x_axis);
-    // add y axis
-    svg
-      .append("g")
-      .attr("transform", `translate(${margin.left},0)`)
-      .call(y_axis);
+      Object.keys(data.data).map((key, i) => {
+        const year = key;
+        const temp = data.data[key];
 
-    // add the line path
-    svg
-      .append("path")
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 2)
-      .attr("d", line_generator(d));
-  });
+        temperatures.push({ year: year, temp: temp });
+      });
+
+      const d = temperatures;
+
+      //min and max for
+      let minYear = d3.min(temperatures, (t) => t.year);
+      let maxYear = d3.max(temperatures, (t) => t.year);
+
+      // x_scale.domain(d3.extent(d, year)).nice(ticks);
+      x_scale.domain([minYear, maxYear]).nice(ticks);
+
+      // y_scale.domain(d3.extent(d, temperature)).nice(ticks);
+      y_scale.domain([d3.min(temperatures, (t) => parseFloat(t.temp)), d3.max(temperatures, (t) => parseFloat(t.temp))]).nice(ticks);
+
+      // append x axis
+      svg
+        .append("g")
+        .attr("transform", `translate(0,${height - margin.bottom - margin.top})`)
+        .call(x_axis);
+      // add y axis
+      svg
+        .append("g")
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(y_axis);
+
+      // add the line path
+      svg
+        .append("path")
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 2)
+        .attr("d", line_generator(d));
+
+  }, []);
 
   return (
     <div className="container">
       <h1 className="heading"> {heading} </h1>
       <DropDown onChange={onChange} data={data.data} />
-      <svg></svg>
+      <svg ref={svgRef}></svg>
     </div>
   );
 }
